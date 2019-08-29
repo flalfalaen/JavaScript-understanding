57dc75350845d5a595d753e5caf5cd936a29ea40
Babaievskyi <OBabaievskyi@kiev.luxoft.com>
2019-08-29 19:22:54 +0300

    playing with context and objects

---
+++ js/context/objectMethodThis.js
@@ -0,0 +1,48 @@
+let person = {
+    firstName: 'John',
+    lastName: "Smith",
+    age: 30,
+
+    greetingInObj_3: function () {
+        console.log('Greetings 3ed time, humans from ' + this.firstName);
+    }
+};
+let admin = {
+    firstName: 'Dexter',
+    lastName: "Doe",
+    age: 28,
+};
+
+//teaching person to say hello///////////////////
+//method 1 //////////////////////////////////////
+person.greetingInObj = function () {
+    console.log('Greetings, humans');
+};
+person.greetingInObj();
+
+//method 2 //////////////////////////////////////
+function greetingFunc() {
+    console.log('Greetings 2nd time, humans');
+}
+person.greetingInObj_2 = greetingFunc;
+person.greetingInObj_2();
+
+//method 3 //////////////////////////////////////
+person.greetingInObj_3();
+
+//improving
+function greetingForAll1() {
+    console.log('Greetings in 4th time, humans ' + this.firstName);
+}
+admin.greetingsInObj_4 = greetingForAll1;
+person.greetingsInObj_4 = greetingForAll1;
+admin.greetingsInObj_4();
+person.greetingsInObj_4();
+
+// let literallyNo1 = person;
+// console.log(literallyNo1.firstName);
+
+
+/////////////////////////////////////////////////
+console.log('\n-----inside of the object-----');
+console.log(person);
--- scratch.js
+++ scratch.js
@@ -21,7 +21,6 @@ console.log(array1);
 //     if (obj.hasOwnProperty(key)) {
 //         console.log('key ' + key + ' value ' + obj[key]);
 //     }
-//
 // }
 
 var val = 2;
@@ -30,24 +29,79 @@ const obj2 = {
     val: 1,
     func: function() {
         function test() {
-            console.log('dddd', this.val);
+            console.log('global a', this.val);
         }
 
         test();
+        console.log('context a', this.val);
     }
 };
-
-
 obj2.func();
 
+function test1 () {
+    var val = 3;
+    console.log('global var is calling inside func test1() {} as window.val = ' + window.val );
+    console.log('global var is calling inside func test1() {} as this.val = ' + this.val);
+    console.log('global var is calling inside func test1() {} as val = ' + val);
+}
+
+// console.log(test1.prototype.constructor);
+// var test44 = new test1();
+// console.log('1111111', test44);
+
+
+var val = 'global 2';
+
+const obj2 = {
+    val: 'obj2 1',
+    func: function() {
+        // val = this.val;
+        function test() {
+            console.log('func()s test() ---' + this.val);
+        }
+
+        test();
+        console.log('func() --- ' + this.val);
+        console.log('innerObj.val2 --- ' + this.innerObj.val2);
+        console.log('innerObj.func2() --- ' + this.innerObj.func2());
+        console.log('func3() --- ' + this.func3());
+    },
+    func3: function() {
+        return this.val;
+    },
+    innerObj: {
+        val2: this.val,
+        func2: function() {
+            function func2_1() {
+                console.log('func2_1 --- ' + this.val);
+            }
+            func2_1();
+            return this.val2;
+        }
+    }
+};
+obj2.func();
+
+
+
+
+
+
+
+
+
 function Animal(name) {
-    this.name;
+    this.name = name;
 }
 
-Animal.prototype.run() {
-
-}
+Animal.prototype.run = function () {
+    console.log(this.name + ' is running');
+};
 
 function Rabbit() {
 
 }
+
+let rabbit = new Animal('monster');
+
+console.log(rabbit.run());
